e8b2275e28f123aff277495d1ca19c59e8a2887b
JongwonYoun <jeffyoun@naver.com>
2021-02-12 17:47:18 +0900

    feat: UserDomain 추가, Repository 추가

---
+++ src/main/java/justmove/domain/user/User.java
@@ -0,0 +1,46 @@
+package justmove.domain.user;
+
+import justmove.domain.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseEntity {
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
